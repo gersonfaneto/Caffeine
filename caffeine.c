7297cdae6bf1514faa3650e85f8d4429e749dd93
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2025-02-11 20:19:03 -0300

    feat: basic structure of the project

---
+++ caffeine.c
@@ -0,0 +1,11 @@
+#define CAFFEINE_IMPLEMENTATION
+#include "caffeine.h"
+
+int main(void)
+{
+    say_hello("World");
+
+    return 0;
+}
+
+// vim: sw=4 ts=4 sts=4 et tw=80 nospell :
---
+++ caffeine.h
@@ -0,0 +1,21 @@
+#ifndef CAFFEINE_H_ #define CAFFEINE_H_
+
+// #include <caffeine/containers.h>
+// #include <caffeine/network.h>
+// #include <caffeine/tui.h>
+// ...
+
+#ifdef CAFFEINE_IMPLEMENTATION
+
+#include <stdio.h>
+
+static inline void say_hello(const char* name)
+{
+    printf("Hello, %s!\n", name);
+}
+
+#endif // ifdef CAFFEINE_IMPLEMENTATION
+
+#endif // ifndef CAFFEINE_H_
+
+// vim: sw=4 ts=4 sts=4 et tw=80 nospell :
