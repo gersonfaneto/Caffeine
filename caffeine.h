8c01850655457b690a96e2940ca984ee77048820
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2025-02-11 20:19:57 -0300

    chore: add license notice per FSF recommendations

--- caffeine.h
+++ caffeine.h
@@ -1,3 +1,6 @@
+// Copyright (C) 2025  Gerson Ferreira <gersonfaneto.dev@gmail.com>
+// See end of file for extended copyright information.
+
 #ifndef CAFFEINE_H_ #define CAFFEINE_H_
 
 // #include <caffeine/containers.h>
@@ -17,5 +20,23 @@ static inline void say_hello(const char* name)
 #endif // ifdef CAFFEINE_IMPLEMENTATION
 
 #endif // ifndef CAFFEINE_H_
+
+// Caffeine :: Boost Your C Development.
+// Copyright (C) 2025  Gerson Ferreira <gersonfaneto.dev@gmail.com>
+//
+// This file is part of Caffeine.
+//
+// Caffeine is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Caffeine is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Caffeine.  If not, see <https://www.gnu.org/licenses/>.
 
 // vim: sw=4 ts=4 sts=4 et tw=80 nospell :
