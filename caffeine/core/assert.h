422a640def83a6808ee1932e6957b391ea843cbd
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2025-02-24 13:10:14 -0300

    fix(core): `assert.h` - `return` can't be used in macros
    The code was copy-pasted from an `inline` function and I forgot to make
    the change :p

--- caffeine/core/assert.h
+++ caffeine/core/assert.h
@@ -10,21 +10,24 @@
 #include <stdlib.h>
 
 #ifndef __FUNCTION__
-  #define __FUNCTION__ __func__
-#endif // ifndef __FUNCTION__
+    #define __FUNCTION__ __func__
+#endif // ifdef __FUNCTION__
 
 #ifdef __DEBUG__
-  #define __ASSERT__(condition, format, ...)     \
-    if ((condition)) return;                     \
-                                                 \
-    fprintf(stderr, format "\n", ##__VA_ARGS__); \
-                                                 \
-    abort();                                     \
-
-  #define __TODO__(...) __ASSERT__(0, "TODO :: `%s` at `%s:%d`", __FUNCTION__, __FILE__, __LINE__)
+    #define __ASSERT__(condition, format, ...)           \
+        do {                                             \
+            if ((condition)) break;                      \
+            fprintf(stderr, format "\n", ##__VA_ARGS__); \
+            abort();                                     \
+        } while (0)
+    #define __TODO__(...)                                \
+        __ASSERT__(0, "TODO :: `%s` at `%s:%d`",         \
+                __FUNCTION__,                            \
+                __FILE__,                                \
+                __LINE__)
 #else
-    #define __ASSERT__(...) ((void) 0)
-    #define __TODO__(...) ((void) 0)
+    #define __TODO__(...)   ((void)0)
+    #define __ASSERT__(...) ((void)0)
 #endif // ifdef __DEBUG__
 
 #endif // ifdef CAFFEINE_ASSERT_IMPLEMENTATION
