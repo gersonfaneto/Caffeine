8d4cc0c0b7ca0cf55e9c686c34e31d737b3624cb
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2025-04-04 07:29:52 -0300

    feat(containers): include type aliases under `list.h`

--- caffeine/containers/list.h
+++ caffeine/containers/list.h
@@ -4,7 +4,9 @@
 #ifndef CAFFEINE_LIST_H
 #define CAFFEINE_LIST_H
 
-#include <stddef.h>
+typedef long unsigned int index_a;
+typedef long unsigned int size_a;
+typedef long unsigned int lenght_a;
 
 typedef struct node_t node_t;
 typedef struct list_t list_t;
@@ -20,18 +22,18 @@ struct node_t
 {
     node_t* __next;
     var     __data;
-    size_t  __size;
+    size_a  __size;
 };
 
 struct list_t
 {
-    node_t* __leader;
-    size_t  __lenght;
+    node_t*   __leader;
+    lenght_a  __lenght;
 
     void    (*clear) (list_t* self);
-    void   (*insert) (list_t* self, var data, size_t size, size_t position);
-    void   (*remove) (list_t* self, size_t position);
-    void (*retrieve) (list_t* self, size_t position, var* store);
+    void   (*insert) (list_t* self, var data, size_a size, index_a index);
+    void   (*remove) (list_t* self, index_a index);
+    void (*retrieve) (list_t* self, index_a index, var* store);
 };
 
 #ifdef CAFFEINE_LIST_IMPLEMENTATION
@@ -42,7 +44,7 @@ struct list_t
 #define CAFFEINE_ASSERT_IMPLEMENTATION
 #include <caffeine/core/assert.h>
 
-static node_t* __node_init__(var data, size_t size)
+static node_t* __node_init__(var data, size_a size)
 {
     node_t* node = (node_t*) malloc(sizeof(node_t));
 
@@ -78,17 +80,17 @@ static void __list_clear__(list_t* self)
     __TODO__();
 }
 
-static void __list_insert__(list_t* self, var data, size_t size, size_t position)
+static void __list_insert__(list_t* self, var data, size_a size, index_a index)
 {
     __TODO__();
 }
 
-static void __list_remove__(list_t* self, size_t position)
+static void __list_remove__(list_t* self, index_a index)
 {
     __TODO__();
 }
 
-static void __list_retrieve__(list_t* self, size_t position, var* store)
+static void __list_retrieve__(list_t* self, index_a index, var* store)
 {
     __TODO__();
 }
@@ -107,7 +109,7 @@ list_t list_init(void)
     };
 }
 
-#endif // ifdef CAFFEINE_LIST_IMPLEMENTATION
+#endif /* ifdef CAFFEINE_LIST_IMPLEMENTATION */
 
 #endif // ifndef CAFFEINE_LIST_H
 
--- examples/list.c
+++ examples/list.c
@@ -1,3 +1,8 @@
+/*
+ * Copyright (C) 2025  Gerson Ferreira <gersonfaneto.dev@gmail.com>
+ * See end of file for extended copyright information.
+ */
+
 #define CAFFEINE_LIST_IMPLEMENTATION
 #include <caffeine/containers/list.h>
 
@@ -5,7 +10,9 @@ int main(void)
 {
     list_t list = list_init();
 
-    for (int i = 0; i < 10; ++i)
+    index_a i;
+
+    for (i = 0; i < 10; ++i)
     {
         int x = (i + 1);
         list.insert(&list, &x, sizeof(x), i);
@@ -13,11 +20,11 @@ int main(void)
 
     printf("list.__lenght :: %lu\n", list.__lenght);
 
-    size_t llenght = list.__lenght;
+    lenght_a llenght = list.__lenght;
 
-    for (size_t i = 0; i < llenght; ++i)
+    for (i = 0; i < llenght; ++i)
     {
-        size_t j = (llenght - i - 1);
+        index_a j = (llenght - i - 1);
         var x = NULL;
         list.retrieve(&list, j, &x); list.remove(&list, j);
         printf("@%lu :: %d\n", j, *(int*) x);
@@ -31,4 +38,24 @@ int main(void)
     return 0;
 }
 
-// vim: sw=4 ts=4 sts=4 et tw=80 nospell ft=c :
+/*
+ * Caffeine :: Boost Your C Development.
+ * Copyright (C) 2025  Gerson Ferreira <gersonfaneto.dev@gmail.com>
+ *
+ * This file is part of Caffeine.
+ *
+ * Caffeine is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Caffeine is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Caffeine.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+/* vim: sw=4 ts=4 sts=4 et tw=80 nospell : */
