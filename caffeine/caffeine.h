a2fe48e6b247bd3c1d2e3f2ac3be68d3dff62c1d
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2025-05-23 09:59:29 -0300

    chore(*)!: swith to 'The Unlicense' for LICENSE

--- caffeine/caffeine.h
+++ caffeine/caffeine.h
@@ -1,6 +1,3 @@
-// Copyright (C) 2025  Gerson Ferreira <gersonfaneto.dev@gmail.com>
-// See end of file for extended copyright information.
-
 #ifndef CAFFEINE_H_
 #define CAFFEINE_H_
 
@@ -22,23 +19,5 @@ static inline void say_hello(const char* name)
 #endif // ifdef CAFFEINE_IMPLEMENTATION
 
 #endif // ifndef CAFFEINE_H_
-
-// Caffeine :: Boost your C development.
-// Copyright (C) 2025  Gerson Ferreira <gersonfaneto.dev@gmail.com>
-//
-// This file is part of Caffeine.
-//
-// Caffeine is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// Caffeine is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with Caffeine.  If not, see <https://www.gnu.org/licenses/>.
 
 // vim: sw=4 ts=4 sts=4 et tw=80 nospell :
--- caffeine/containers/list.h
+++ caffeine/containers/list.h
@@ -1,6 +1,3 @@
-// Copyright (C) 2025  Gerson Ferreira <gersonfaneto.dev@gmail.com>
-// See end of file for extended copyright information.
-
 #ifndef CAFFEINE_LIST_H
 #define CAFFEINE_LIST_H
 
@@ -109,26 +106,8 @@ list_t list_init(void)
     };
 }
 
-#endif /* ifdef CAFFEINE_LIST_IMPLEMENTATION */
+#endif // ifdef CAFFEINE_LIST_IMPLEMENTATION
 
 #endif // ifndef CAFFEINE_LIST_H
-
-// Caffeine :: Boost your C development.
-// Copyright (C) 2025  Gerson Ferreira <gersonfaneto.dev@gmail.com>
-//
-// This file is part of Caffeine.
-//
-// Caffeine is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// Caffeine is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with Caffeine.  If not, see <https://www.gnu.org/licenses/>.
 
 // vim: sw=4 ts=4 sts=4 et tw=80 nospell :
